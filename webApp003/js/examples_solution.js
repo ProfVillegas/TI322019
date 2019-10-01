c4f98f7ffe445b608fffb0a41650b3d6f4e2796c
ProfVillegas <julian.villegas@utrivieramaya.edu.mx>
2019-10-01 11:05:45 -0500

    final

--- webApp003/js/examples_solution.js
+++ webApp003/js/examples_solution.js
@@ -10,10 +10,33 @@ function magic_color(current) {
 
 function bar(class_value, element, width) {
     var current = document.getElementsByClassName(class_value)[element];
-    current.style.width = width + "px";
+    current.style.width = width + "%";
 }
 
 function show_clue() {
     var show_element = document.getElementsByClassName("clue")[0];
     show_element.style.display = "block";
+}
+
+function is_odd(num) {
+    var text = "";
+    if (num % 2 == 0) {
+        text = num + " is not odd ";
+    } else {
+        text = num + " is odd ";
+    }
+    document.getElementsByClassName("rs")[0].innerHTML = text;
+}
+
+function volumen(r, h) {
+    var result = 3.1416 * (r * r) * h;
+    document.getElementsByClassName("rs_volumen")[0].innerHTML = result.toFixed(2);
+}
+
+function draw_bars(m, w) {
+    var total = parseFloat(m) + parseFloat(w);
+    var porcent_m = (100 / total) * m;
+    var porcent_w = (100 / total) * w;
+    bar("bar-m", 0, porcent_m);
+    bar("bar-w", 0, porcent_w);
 }
